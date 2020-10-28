d16329c30835cd0a04e5bcc68a1f6d91d4458b20
Mathias Myrland <jedimemo@gmail.com>
2020-10-28 23:55:56 +0100

    (initial-code) working on navigation drawers

--- examples/component_showcase/src/components/navigation_drawer_demo.rs
+++ examples/component_showcase/src/components/navigation_drawer_demo.rs
@@ -1,6 +1,8 @@
 use dominator::{Dom, html};
+use futures_signals::signal::Mutable;
+use futures_signals::signal::SignalExt;
 
-use dominator_material::components::{Card, NavigationDrawer};
+use dominator_material::components::{Card, NavigationDrawer, NavigationDrawerEntry};
 
 pub struct NavigationDrawerDemo {}
 
@@ -10,11 +12,25 @@ impl NavigationDrawerDemo {
     }
 
     pub fn render(self) -> Dom {
+        let f = web_sys::DocumentFragment::new();
+        let current_view = Mutable::new(0);
+
         Card::new()
             .body(html!("div", {
                 .class("navigation-drawer-demo")
                 .child(NavigationDrawer::new()
-                        .main(Card::new().render()).render())
+                    .connect(|v| {
+                        v.set_entries(vec![
+                            NavigationDrawerEntry::Item,
+                            NavigationDrawerEntry::Item,
+                            NavigationDrawerEntry::Item,
+                            NavigationDrawerEntry::Item,
+                        ])
+                    })
+                    .main(current_view.signal_cloned().map(|v| {
+                        Some(html!("span", { .text("Main view") }))
+                    }))
+                    .render())
             }))
             .render()
     }
--- src/components/navigation_drawer.rs
+++ src/components/navigation_drawer.rs
@@ -1,33 +1,88 @@
+use std::borrow::BorrowMut;
+use std::ops::DerefMut;
+
 use dominator::{Dom, html};
+use futures_signals::signal::{Mutable, Signal};
+use futures_signals::signal_vec::MutableVec;
+use futures_signals::signal_vec::SignalVecExt;
+use wasm_bindgen::__rt::core::cell::RefCell;
+use wasm_bindgen::__rt::core::pin::Pin;
+use wasm_bindgen::__rt::std::rc::Rc;
+
+#[derive(Clone)]
+pub enum NavigationDrawerEntry {
+    Item
+}
+
+pub struct NavigationDrawerData {
+    main_signal: Option<Pin<Box<dyn Signal<Item=Option<Dom>>>>>,
+    entries: MutableVec<NavigationDrawerEntry>,
+}
+
+impl NavigationDrawerData {
+    pub fn set_entries(&self, entries: Vec<NavigationDrawerEntry>) {
+        self.entries.lock_mut().replace_cloned(entries);
+    }
+}
 
 pub struct NavigationDrawer {
-    main: Option<Dom>
+    state_handler: Option<Box<dyn FnOnce(Rc<NavigationDrawerData>)>>,
+    data: NavigationDrawerData,
 }
 
 impl NavigationDrawer {
     pub fn new() -> NavigationDrawer {
-        NavigationDrawer { main: None }
+        NavigationDrawer {
+            state_handler: None,
+            data: NavigationDrawerData {
+                main_signal: None,
+                entries: Default::default(),
+            },
+        }
     }
 
     #[inline]
-    pub fn main(mut self, dom: Dom) -> Self {
-        self.main = Some(html!("div", {
-            .class("dmat-navigation-drawer-main")
-            .class("dmat-surface")
-            .child(dom)
-        }));
+    pub fn main<S>(mut self, main_signal: S) -> Self where S: Signal<Item=Option<Dom>> + 'static {
+        self.data.main_signal = Some(Box::pin(main_signal));
         self
     }
 
-    pub fn render(self) -> Dom {
-        html!("div", {
-            .class("dmat-navigation-drawer-container")
-            .children(vec![
-                Some(html!("div", {
-                    .class("dmat-navigation-drawer")
-                })),
-                self.main
-            ].into_iter().filter_map(|v| v))
+    #[inline]
+    pub fn connect<F: 'static>(mut self, state_handler: F) -> Self where F: FnOnce(Rc<NavigationDrawerData>) {
+        self.state_handler = Some(Box::new(state_handler));
+        self
+    }
+
+    pub fn render(mut self) -> Dom {
+        let s = Rc::new(self.data);
+
+        if let Some(state_handler) = self.state_handler {
+            state_handler(s.clone())
+        }
+
+        Dom::with_state(s, |s| {
+            html!("div", {
+                .class("dmat-navigation-drawer-container")
+                .children(vec![
+                    Some(html!("div", {
+                        .class("dmat-navigation-drawer")
+                        .child(html!("div", {
+                            .class("drawer-container")
+                            .children_signal_vec(s.entries.signal_vec_cloned().map(|entry| {
+                                html!("div", { .class("dmat-navigation-drawer-entry") .text("Some entry") })
+                            }))
+                        }))
+                    })),
+                    match Rc::get_mut(s).unwrap().main_signal.take() {
+                        Some(sig) => Some(html!("div", {
+                            .class("dmat-navigation-drawer-main")
+                            .class("dmat-surface")
+                            .child_signal(sig)
+                        })),
+                        _ => None
+                    }
+                ].into_iter().filter_map(|v| v))
+            })
         })
     }
 }
