3268d6ca94db2442fb78375f90b51914a70a0b55
Mathias Myrland <jedimemo@gmail.com>
2021-12-27 11:03:34 +0100

    update dicon

--- src/components/icon.rs
+++ src/components/icon.rs
@@ -1,3 +1,4 @@
+use dominator::traits::AsStr;
 use dominator::{html, Dom, DomBuilder};
 use web_sys::HtmlElement;
 
@@ -9,58 +10,43 @@ pub enum IconSize {
     ExtraLarge,
 }
 
+#[inline]
 /// Render an icon using iconfify
 /// see https://iconify.design/ for more information on usage and available icon libraries
-pub struct Icon {
-    size: IconSize,
-    icon_name: String,
+///
+/// The apply function will be called with the containers DomBuilder, allowing
+/// customization
+///
+/// icon_name must be a valid iconify selector, on the form {collection}:{icon-name}
+pub fn icon<T: AsStr>(size: IconSize, icon_name: T) -> Dom {
+    icon_with_apply(size, icon_name, |d| d)
 }
 
-impl Icon {
-    /// Construct a new Icon.
-    ///
-    /// icon_name must be a valid iconify selector, on the form {collection}:{icon-name}
-    ///
-    /// ```
-    /// use dominator_material::components::{Icon, IconSize};
-    /// let dom = Icon::new("ic::baseline-home", IconSize::Default).render();
-    /// ```
-    pub fn new<T1: ToString>(icon_name: T1, size: IconSize) -> Icon {
-        Icon {
-            size,
-            icon_name: icon_name.to_string(),
-        }
-    }
+#[inline]
+/// Render an icon using iconfify
+/// see https://iconify.design/ for more information on usage and available icon libraries
+///
+/// The apply function will be called with the containers DomBuilder, allowing
+/// customization
+/// icon_name must be a valid iconify selector, on the form {collection}:{icon-name}
+pub fn icon_with_apply<F, T: AsStr>(size: IconSize, icon_name: T, apply: F) -> Dom
+where
+    F: FnOnce(DomBuilder<HtmlElement>) -> DomBuilder<HtmlElement> + 'static,
+{
+    let size_class = match size {
+        IconSize::Default => "-default",
+        IconSize::Normal => "-normal",
+        IconSize::Large => "-large",
+        IconSize::ExtraLarge => "-extra-large",
+        IconSize::Small => "-small",
+    };
 
-    #[inline]
-    pub fn render(self) -> Dom {
-        self.render_apply(|v| v)
-    }
-
-    #[inline]
-    /// Render the Icon
-    ///
-    /// The apply function will be called with the containers DomBuilder, allowing
-    /// customization
-    pub fn render_apply<F>(self, apply: F) -> Dom
-    where
-        F: FnOnce(DomBuilder<HtmlElement>) -> DomBuilder<HtmlElement> + 'static,
-    {
-        let size_class = match self.size {
-            IconSize::Default => "-default",
-            IconSize::Normal => "-normal",
-            IconSize::Large => "-large",
-            IconSize::ExtraLarge => "-extra-large",
-            IconSize::Small => "-small",
-        };
-
-        html!("span", {
-            .class("iconify")
-            .class("dmat-icon")
-            .class(size_class)
-            .apply(apply)
-            .attribute("data-icon", format!("{}",self.icon_name ).as_str())
-            .attribute("data-inline", "false")
-        })
-    }
+    html!("span", {
+        .class("iconify")
+        .class("dmat-icon")
+        .class(size_class)
+        .apply(apply)
+        .attribute("data-icon", format!("{}", icon_name.as_str()).as_str())
+        .attribute("data-inline", "false")
+    })
 }
