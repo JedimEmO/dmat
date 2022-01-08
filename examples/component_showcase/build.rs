f9a1c28f9eac954722d2229addeaa2e57c1ff47b
Mathias Myrland <jedimemo@gmail.com>
2022-01-08 14:10:36 +0100

    undo default breaking

--- examples/component_showcase/build.rs
+++ examples/component_showcase/build.rs
@@ -1,16 +1,16 @@
-use std::fs;
-
 use dominator_material_style::render_dmat_scss;
 use dominator_material_style::theme::breakpoints::BreakpointValue;
 use dominator_material_style::theme::dmat_theme::DmatTheme;
 use dominator_material_style::theme::layout::{DisplayUnit, Layout};
+use std::fs;
 
 fn main() {
     let theme = DmatTheme {
         layout: Layout {
             margin: BreakpointValue {
+                small: DisplayUnit::Pixels(8),
+                medium: DisplayUnit::Pixels(16),
                 large: DisplayUnit::Pixels(32),
-                ..Default::default()
             },
         },
         ..Default::default()
