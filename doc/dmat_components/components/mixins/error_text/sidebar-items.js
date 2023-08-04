cb8a60e6dbd4533ac5ad647d6dc0f4047df9801c
JedimEmO <jedimemo@gmail.com>
2023-08-04 23:26:11 +0000

    Deploying to gh-pages from @ JedimEmO/dmat@1f11aef89036ab76ac6970583cc79cce4242b9e6 🚀

--- doc/dmat_components/components/mixins/error_text/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["error_text"]};
--- doc/dmat_components/components/mixins/sidebar-items.js
+++ doc/dmat_components/components/mixins/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["assistive_text","children_builder","disabled_signal","error_text","invalid_signal"]};
+window.SIDEBAR_ITEMS = {"mod":["assistive_text","children_builder","disabled_signal","invalid_signal"]};
